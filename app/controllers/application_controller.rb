ea7f69bfcd85a9a98fe385502368c0acd2089134
Dariusz Krol <dkrol@agh.edu.pl>
2014-03-12 12:02:42 +0100

    Updating authentication process

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,29 +5,72 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  # should return true if OK or false if NOT OK
   def authenticate
+    @current_user = nil; @sm_user = nil
 
-    if request.env.include?('HTTP_SSL_CLIENT_S_DN') and
-        request.env['HTTP_SSL_CLIENT_S_DN'] != '(null)' and
-        request.env['HTTP_SSL_CLIENT_VERIFY'] == 'SUCCESS'
+    Rails.logger.debug("Session: #{session[:user]}")
 
-      puts "We can use DN(#{request.env['HTTP_SSL_CLIENT_S_DN']}) for authentication"
-      scalarm_user = ScalarmUser.find_by_dn(request.env['HTTP_SSL_CLIENT_S_DN'])
+    if session[:user]
+      Rails.logger.debug('[authentication] using session data')
 
-      return true if scalarm_user.nil?
-    end
+      @current_user = ScalarmUser.find_by_id(session[:user])
+      authentication_failed if @current_user.blank?
 
-    if request.env.include?('HTTP_AUTHORIZATION') and request.env['HTTP_AUTHORIZATION'].include?('Basic')
-      authenticate_or_request_with_http_basic do |sm_uuid, password|
-        Rails.logger.debug("Possible SM authentication: #{sm_uuid}")
-        temp_pass = SimulationManagerTempPassword.find_by_sm_uuid(sm_uuid)
+    else
+      # if the user provides a valid certificate
+      if request.env.include?('HTTP_SSL_CLIENT_S_DN') and request.env['HTTP_SSL_CLIENT_S_DN'] != '(null)' and request.env['HTTP_SSL_CLIENT_VERIFY'] == 'SUCCESS'
+        Rails.logger.debug("[authentication] using DN: '#{request.env['HTTP_SSL_CLIENT_S_DN']}'")
 
-        return true if (not temp_pass.nil?) and temp_pass.password == password
+        begin
+          session[:user] = ScalarmUser.authenticate_with_certificate(request.env['HTTP_SSL_CLIENT_S_DN']).id
+          @current_user = ScalarmUser.find_by_id(session[:user])
+          flash[:notice] = t('login_success')
+          true
+        rescue Exception => e
+          reset_session
+          flash[:error] = e.to_s
+
+          false
+        end
+
+      # if the user provides user and password with basic auth -> it can be either user or Simulation manager
+      elsif request.env.include?('HTTP_AUTHORIZATION') and request.env['HTTP_AUTHORIZATION'].include?('Basic')
+
+        authenticate_or_request_with_http_basic do |sm_uuid, password|
+          temp_pass = SimulationManagerTempPassword.find_by_sm_uuid(sm_uuid)
+
+          unless temp_pass.nil?
+            Rails.logger.debug("[authentication] SM using uuid: '#{sm_uuid}'")
+            correct = ((not temp_pass.nil?) and (temp_pass.password == password))
+            @sm_user = temp_pass if correct
+
+            correct
+          else
+            Rails.logger.debug("[authentication] using login: '#{sm_uuid}'")
+            @current_user = ScalarmUser.authenticate_with_password(sm_uuid, password)
+          end
+
+        end
+
+      else
+        authentication_failed
+        false
       end
+
     end
 
-    false
+  end
+
+  def authentication_failed
+    reset_session
+    flash[:error] = 'You have to login first'
+
+    Rails.logger.debug('[authentication] failed -> redirect')
+
+    session[:intended_action] = action_name
+    session[:intended_controller] = controller_name
+
+    #redirect_to :login
   end
 
 end
